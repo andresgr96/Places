2f7d1df55fe07147468ea31b516ef8c20752c395
Andres Garcia and Nicolas Hurtado <andres@>
2016-08-22 11:09:53 -0400

    create a new directory with an html form asking the user to input information about places visited

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business Logic
+function Place(location, landmark, season, notes, tags) {
+  this.placeLocatopn = location;
+  this.placelandmark = landmak;
+  this.placeSeason = season;
+  this.placeNotes = notes;
+  this.place.Tags = tags;
+}
+
+// user interface logic
