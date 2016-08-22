3b205f248bd211ff99714e523e76d7b1fde60ab4
Andres Garcia and Nicolas Hurtado <andres@>
2016-08-22 11:50:42 -0400

    added business and user interface logic to get the program to store the places visited on a list and diplay more information when clicked on the list

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,47 @@
 // Business Logic
 function Place(location, landmark, season, notes, tags) {
-  this.placeLocatopn = location;
-  this.placelandmark = landmak;
+  this.placeLocation = location;
+  this.placeLandmark = landmark;
   this.placeSeason = season;
   this.placeNotes = notes;
-  this.place.Tags = tags;
+  this.placeTags = tags;
+}
+
+Place.prototype.notesTags = function() {
+  return this.placeNotes + " Tags: " + this.placeTags;
 }
 
 // user interface logic
+
+$(document).ready(function(){
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedLocation = $("input#new-location").val();
+    var inputtedLandmark = $("input#new-landmarks").val();
+    var inputtedSeason = $("input#new-season").val();
+    var inputtedNotes = $("input#new-notes").val();
+    var inputtedTags = $("input#new-tags").val();
+
+    var newPlace = new Place(inputtedLocation, inputtedLandmark, inputtedSeason, inputtedNotes, inputtedTags);
+
+  $("ul#places").append("<li><span class = 'place'>" + inputtedLocation + "</span></li>");
+
+  $(".place").last().click(function () {
+    $("#show-location").show();
+    $("#show-location h2").text(newPlace.placeLocation);
+    $(".location").text(newPlace.placeLocation);
+    $(".landmark").text(newPlace.placeLandmark);
+    $(".season").text(newPlace.placeSeason);
+    $(".notes-tags").text(newPlace.notesTags());
+  });
+
+
+  $("input#new-location").val("");
+  $("input#new-landmarks").val("");
+  $("input#new-season").val("");
+  $("input#new-notes").val("");
+  $("input#new-tags").val("");
+  });
+
+});
